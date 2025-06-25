1e85efddf47242a57b00933271685f4d00750aae
Alejandro <ariel10aguero@gmail.com>
2025-06-25 14:52:50 -0300

    Include vlc on build

--- common/src/commonMain/kotlin/chat/sphinx/common/components/media_player/MediaPlayerControllerImpl.kt
+++ common/src/commonMain/kotlin/chat/sphinx/common/components/media_player/MediaPlayerControllerImpl.kt
@@ -3,25 +3,39 @@ package chat.sphinx.common.components.media_player
 import uk.co.caprica.vlcj.factory.MediaPlayerFactory
 import uk.co.caprica.vlcj.player.base.MediaPlayer
 import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter
+import java.io.File
 
 class MediaPlayerControllerImpl : MediaPlayerController() {
 
-    private val mediaPlayerFactory = MediaPlayerFactory()
-    private val mediaPlayer: MediaPlayer = mediaPlayerFactory.mediaPlayers().newMediaPlayer()
+    private val mediaPlayerFactory: MediaPlayerFactory
+    private val mediaPlayer: MediaPlayer
 
     private var playbackListener: MediaPlayerController.PlaybackListener? = null
 
     init {
+        try {
+            val nativeDir = File(
+                MediaPlayerControllerImpl::class.java.getResource("/natives/windows/x86_64")!!.toURI()
+            )
+            System.setProperty("jna.library.path", nativeDir.absolutePath)
+            System.setProperty("VLC_PLUGIN_PATH", File(nativeDir, "plugins").absolutePath)
+        } catch (e: Exception) {
+            println("VLC path setup failed: ${e.message}")
+        }
+
+        mediaPlayerFactory = MediaPlayerFactory()
+        mediaPlayer = mediaPlayerFactory.mediaPlayers().newMediaPlayer()
+
         mediaPlayer.events().addMediaPlayerEventListener(object : MediaPlayerEventAdapter() {
             override fun finished(mediaPlayer: MediaPlayer?) {
                 playbackListener?.onPlaybackCompleted()
             }
         })
     }
+
     override fun setPlaybackListener(listener: MediaPlayerController.PlaybackListener?) {
         playbackListener = listener
     }
-
 
     override fun play(url: String, startTimeMillis: Long) {
         mediaPlayer.media().play(url)
