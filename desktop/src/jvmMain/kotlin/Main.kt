1f596c81e31c1e98c206ad62bbb214f39e66b301
Alejandro <ariel10aguero@gmail.com>
2025-06-25 12:43:22 -0300

    Add logs on a text file

--- desktop/src/jvmMain/kotlin/Main.kt
+++ desktop/src/jvmMain/kotlin/Main.kt
@@ -33,9 +33,14 @@ import kotlinx.coroutines.withContext
 import theme.LocalSpacing
 import theme.Spacing
 import java.io.File
+import java.io.PrintStream
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() = application {
+    val logFile = File("sphinx-log.txt")
+    System.setOut(PrintStream(logFile))
+    System.setErr(PrintStream(logFile))
+
     val windowState = rememberWindowState()
     val sphinxIcon = imageResource(DesktopResource.drawable.sphinx_icon)
 
