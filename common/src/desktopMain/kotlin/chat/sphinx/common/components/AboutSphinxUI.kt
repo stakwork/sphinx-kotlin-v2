fdfeaabb6b64722f45c563a53be5788df2b3542e
Alejandro <ariel10aguero@gmail.com>
2025-07-25 11:53:12 -0300

    Version 1.0.11

--- common/src/desktopMain/kotlin/chat/sphinx/common/components/AboutSphinxUI.kt
+++ common/src/desktopMain/kotlin/chat/sphinx/common/components/AboutSphinxUI.kt
@@ -62,7 +62,7 @@ fun AboutSphinx(dashboardViewModel: DashboardViewModel) {
                 Spacer(Modifier.height(8.dp))
 
                 Text(
-                    text = "Version " + "1.0.10",
+                    text = "Version " + "1.0.11",
                     color = MaterialTheme.colorScheme.tertiary,
                     fontFamily = Roboto,
                     fontWeight = FontWeight.Light,
--- common/src/desktopMain/kotlin/chat/sphinx/common/components/DashboardSidebarUI.kt
+++ common/src/desktopMain/kotlin/chat/sphinx/common/components/DashboardSidebarUI.kt
@@ -438,7 +438,7 @@ fun DashboardSidebarUI(
                     Spacer(modifier = Modifier.height(16.dp))
 
                     Text(
-                        text = "Version 1.0.10",
+                        text = "Version 1.0.11",
                         style = androidx.compose.material3.MaterialTheme.typography.bodySmall,
                         color = MaterialTheme.colorScheme.tertiary,
                         modifier = Modifier.align(Alignment.Start)
