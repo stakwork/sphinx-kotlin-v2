0df17577b684df942f9c32373ffbbd02b41ebb61
Alejandro <ariel10aguero@gmail.com>
2025-08-08 11:53:34 -0300

    Version 1.0.12

--- common/src/desktopMain/kotlin/chat/sphinx/common/components/AboutSphinxUI.kt
+++ common/src/desktopMain/kotlin/chat/sphinx/common/components/AboutSphinxUI.kt
@@ -62,7 +62,7 @@ fun AboutSphinx(dashboardViewModel: DashboardViewModel) {
                 Spacer(Modifier.height(8.dp))
 
                 Text(
-                    text = "Version " + "1.0.11",
+                    text = "Version " + "1.0.12",
                     color = MaterialTheme.colorScheme.tertiary,
                     fontFamily = Roboto,
                     fontWeight = FontWeight.Light,
--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -108,7 +108,7 @@ compose.desktop {
 
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "Sphinx"
-            packageVersion = "1.0.11"
+            packageVersion = "1.0.12"
 
             val sphinxProperties = Properties().apply {
                 val defaultsFile = project.file("../local.defaults.properties")
