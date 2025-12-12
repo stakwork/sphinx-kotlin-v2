e4a710deeecbe2a1fa877ccc989db87da6091d79
Alejandro <ariel10aguero@gmail.com>
2025-12-12 12:47:28 -0300

    Version 1.0.23

--- common/src/desktopMain/kotlin/chat/sphinx/common/components/DashboardSidebarUI.kt
+++ common/src/desktopMain/kotlin/chat/sphinx/common/components/DashboardSidebarUI.kt
@@ -442,7 +442,7 @@ fun DashboardSidebarUI(
                     Spacer(modifier = Modifier.height(16.dp))
 
                     Text(
-                        text = "Version 1.0.22",
+                        text = "Version 1.0.23",
                         style = androidx.compose.material3.MaterialTheme.typography.bodySmall,
                         color = MaterialTheme.colorScheme.tertiary,
                         modifier = Modifier.align(Alignment.Start)
