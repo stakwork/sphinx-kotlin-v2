36944889d62def05d52df5597bdd702ff926aabb
Alejandro <ariel10aguero@gmail.com>
2025-12-18 13:37:01 -0300

    - Debug notification sound playing multiple simultaneously on init - Implement playback prevention using mutex - Add isPlaying flag to track current sound state - Implement return logic to skip sound when already playing

--- common/src/desktopMain/kotlin/chat/sphinx/common/components/notifications/DesktopNotificationSoundPlayer.kt
+++ common/src/desktopMain/kotlin/chat/sphinx/common/components/notifications/DesktopNotificationSoundPlayer.kt
@@ -1,6 +1,8 @@
 package chat.sphinx.common.components.notifications
 
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import kotlinx.coroutines.withContext
 import java.io.BufferedInputStream
 import javax.sound.sampled.AudioSystem
@@ -8,23 +10,41 @@ import javax.sound.sampled.Clip
 import javax.sound.sampled.LineEvent
 
 object DesktopNotificationSoundPlayer {
+    private val playbackMutex = Mutex()
+    private var isPlaying = false
+    private var currentClip: Clip? = null
 
     suspend fun playNotificationSound() {
+
+        if (isPlaying) {
+            return
+        }
+
+        playbackMutex.withLock {
+            if (isPlaying) {
+                return
+            }
+            isPlaying = true
+        }
+
         withContext(Dispatchers.IO) {
             try {
                 val audioInputStream = javaClass.getResourceAsStream("/notification.wav")
-                    ?: throw IllegalStateException("Could not find triangle_short.wav in resources")
+                    ?: throw IllegalStateException("Could not find notification.wav in resources")
 
                 val bufferedInputStream = BufferedInputStream(audioInputStream)
                 val audioStream = AudioSystem.getAudioInputStream(bufferedInputStream)
 
                 val clip: Clip = AudioSystem.getClip()
+                currentClip = clip
                 clip.open(audioStream)
 
                 clip.addLineListener { event ->
                     if (event.type == LineEvent.Type.STOP) {
                         clip.close()
                         audioStream.close()
+                        currentClip = null
+                        isPlaying = false
                     }
                 }
 
@@ -32,25 +52,36 @@ object DesktopNotificationSoundPlayer {
 
             } catch (e: Exception) {
                 e.printStackTrace()
+                isPlaying = false
+                currentClip = null
             }
         }
     }
 
     fun playNotificationSoundSync() {
+        if (isPlaying) {
+            return
+        }
+
+        isPlaying = true
+
         try {
             val audioInputStream = javaClass.getResourceAsStream("/notification.wav")
-                ?: throw IllegalStateException("Could not find triangle_short.wav in resources")
+                ?: throw IllegalStateException("Could not find notification.wav in resources")
 
             val bufferedInputStream = BufferedInputStream(audioInputStream)
             val audioStream = AudioSystem.getAudioInputStream(bufferedInputStream)
 
             val clip: Clip = AudioSystem.getClip()
+            currentClip = clip
             clip.open(audioStream)
 
             clip.addLineListener { event ->
                 if (event.type == LineEvent.Type.STOP) {
                     clip.close()
                     audioStream.close()
+                    currentClip = null
+                    isPlaying = false
                 }
             }
 
@@ -58,6 +89,19 @@ object DesktopNotificationSoundPlayer {
 
         } catch (e: Exception) {
             e.printStackTrace()
+            isPlaying = false
+            currentClip = null
+        }
+    }
+
+    fun stopCurrentSound() {
+        currentClip?.let { clip ->
+            if (clip.isRunning) {
+                clip.stop()
+            }
+            clip.close()
+            currentClip = null
+            isPlaying = false
         }
     }
 }
